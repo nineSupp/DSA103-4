03bf78dae6e956ab53ad91a70b2470a84df532fa
ninesri <suppatouch.srn@gmail.com>
2024-07-13 10:32:44 +0700

    commit latest

--- Exercises/Laboratory/Lab2.java
+++ Exercises/Laboratory/Lab2.java
@@ -4,8 +4,10 @@ package Exercises.Laboratory;
 public class Lab2 {
     public static void main(String[] args) {
         // Problem 2.
-        System.out.print("사건은 다가와, ah-oh, ayy\n거세게 커져가, ah-oh, ayy\nThat tick, that tick, tick bomb\nThat tick, that tick, tick bomb\n감히 건드리지 못할 걸 (누구도 말이야)\n지금 내 안에선 (su-su-su-supernova)");
+        System.out.println("사건은 다가와, ah-oh, ayy\n거세게 커져가, ah-oh, ayy\nThat tick, that tick, tick bomb\nThat tick, that tick, tick bomb\n감히 건드리지 못할 걸 (누구도 말이야)\n지금 내 안에선 (su-su-su-supernova)");
 
+        System.out.println(" ");
+    
         // Problem 4.
         int A = 5;
         int B = 11;
@@ -20,6 +22,56 @@ public class Lab2 {
         double Y = F + G;
 
         System.out.println(A + B * C / X - Y - H);
+
+        System.out.println(" ");
+
+        // Problem 5.
+        System.out.println(Math.E);
+        System.out.println(Math.PI);
+        System.out.println(Math.sqrt(36));
+        System.out.println(Math.pow(10, 2));
+        System.out.println(Math.ceil(3.5));
+        System.out.println(Math.abs(-3));
+        System.out.println(Math.round(3.5));
+        System.out.println(Math.floor(3.5));
+        System.out.println(Math.max(3, 4));
+        System.out.println(Math.random());
+        System.out.println(Math.min(3, 4));
+        System.out.println(Math.log(3));
+        System.out.println(Math.exp(10));
+
+        System.out.println(" ");
+
+        // Problem 6.
+        double celsius = 32.0;
+        double fahrenheit = (celsius * (9.0/5.0)) + 32.0;
+        System.out.println(fahrenheit);
+
+        System.out.println(" ");
+
+        // Problem 7.
+        double fahrenheit_7 = 80.6;
+        double celsius_7 = (fahrenheit_7 - 32.0) * (5.0/9.0);
+        System.out.println(Math.round(celsius_7));
+
+        System.out.println(" ");
+
+        // Problem 8.
+        int a = 1;
+        int b = 3;
+        int c = -4;
+        double p = Math.sqrt(Math.pow(b, 2) - (4 * a * c));
+        double x1 = ((-1.0 * b) + (p)) / (2 * a);
+        double x2 = ((-1.0 * b) - (p)) / (2 * a);
+        System.out.println(x1);
+        System.out.println(x2);
+
+        System.out.println(" ");
+
+        // Problem 9.
+        double kg = 68.2;
+        double height = 1.73;
+        double bmi = (kg / Math.pow(height, 2));
+        System.out.println(bmi);
     }
-    
 }
--- Exercises/Week2/ICS21.java
+++ Exercises/Week2/ICS21.java
@@ -14,24 +14,24 @@ public class ICS21 {
 
     public static void main(String[] args) {
         // 1. Store the age of a person.
-        int age = 34;
+        // int age = 34;
         // 2. Store the population of a city.
-        int population = 73_213_497;
+        // int population = 73_213_497;
         // 3. Store the value of π (pi) accurately.
-        double pi = 3.14;
+        // double pi = 3.14;
         // 4. Store a single character 'A'.
-        char character = 'A';
+        // char character = 'A';
         // 5. Store the name of a book.
-        String bookName = "Harry Potter";
+        // String bookName = "Harry Potter";
         // 6. Store whether a light is on or off.
-        boolean lightsOn = true;
+        // boolean lightsOn = true;
         // 7. Store the annual salary of an employee.
-        double emSalary = 34_000.00;
+        // double emSalary = 34_000.00;
         // 8. Store the distance between two cities in kilometers.
-        int distanceCities = 34;
+        // int distanceCities = 34;
         // 9. Store the price of an item in a store with two decimal points.
-        double priceItem = 12.44;
+        // double priceItem = 12.44;
         // 10. Store the number of students in a classroom.
-        int numStudents = 26;
+        // int numStudents = 26;
     }
 }
